73c70e5aa51b37c9ea69f0a95bd7eae1d3721a64
Ju4nCa <ballesteros.juan@correounivalle.edu.co>
2020-03-28 13:14:11 -0500

    multiples clientes

--- Cliente.py
+++ Cliente.py
@@ -1,36 +1,41 @@
 import socket
 import os
+import threading
 
-HOST= '192.168.1.52'
-PORT= 8000
+def cliente():
+    HOST= '192.168.1.52'
+    PORT= 8000
 
-con = socket.socket()
-data=""
+    con = socket.socket()
+    data=""
 
-try:
-    con.connect((HOST, PORT))
-    os.system('color e')
-    os.system('cls')
-    print("***Conectado***")
-    
-except:
-    print("***Servidor caido***")
-while True:
-    
     try:
-        get = input("Mensaje Cliente a Servidor >> ")
-        if get=='exit':
-            os.system('color 7')
-            os.system('cls')
-            break
-        con.send(bytes(get, 'utf8'))
-        data = con.recv(1024)
-        print(data)
-    except:
-        print("***Cliente caido***")
-        os.system('color')
-        os.system('exit')
+        con.connect((HOST, PORT))
+        os.system('color e')
+        os.system('cls')
+        print("***Conectado***")
         
-con.close()
-print("Conexion cerrada")
-os.system('color')
+    except:
+        print("***Servidor caido***")
+    while True:
+        
+        try:
+            get = input("Mensaje Cliente a Servidor >> ")
+            if get=='exit':
+                os.system('color 7')
+                os.system('cls')
+                break
+            con.send(bytes(get, 'utf8'))
+            data = con.recv(1024)
+            print(data)
+        except:
+            print("***Cliente caido***")
+            os.system('color')
+            os.system('exit')
+            
+    con.close()
+    print("Conexion cerrada")
+    os.system('color')
+cliente()
+####hilo1 = threading.Thread(target=cliente)
+##hilo1.start()
--- Servidor.py
+++ Servidor.py
@@ -1,47 +1,47 @@
 import socket
 import os
 import threading
+import time
 
 ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 ser.bind(('', 8000))
 
-ser.listen(2)
-get="" 
+
+ser.listen(2) 
 client = []
 
 os.system('color 9')
 os.system('cls')
 
-for i in range (0, 2):
-    print("Esperando clientes")
-    cli, addr = ser.accept()
-    client.append(cli)
-    print("Conexion aceptada")
-    print("recibido de la IP:  " + str(addr[0]) + " Puerto: " + str(addr[1]))
-i=0
-while True:
-    if i==2:
-        i=0
-    try:
-        get = client[i].recv(1024)
-        print(get)
-        client[i].send(bytes("Recibido", 'utf8'))
-    except:
-        print("***Cliente caido***")
-        print("Esperando clientes")
+def agregar(espacio):
+    while 0<espacio:
+        print("***Esperando clientes || Espacio para: "+ str(espacio) +"***")
+        espacio-=1
         cli, addr = ser.accept()
-        client[i]=cli
-        print("Conexion aceptada")
-        print("recibido de la IP:  " + str(addr[0]) + " Puerto: " + str(addr[1]))            
-    i+=1
-cli.close()
-ser.close()
-print("Conexiones cerradas")
+        client.append(cli)
+        print("***Conexion aceptada***")
+        print("recibido de la IP:  " + str(addr[0]) + " Puerto: " + str(addr[1]))
+    print("***Server Closed***")
+    ser.close()
+    
+hilo_agregar=threading.Thread(target=agregar, args=(2,))
+hilo_agregar.start()
 
+def aceptar(i):
+    while True:
+        try:
+            get = client[i].recv(1024)
+            print(get)
+            client[i].send(bytes("Recibido", 'utf8'))
+        except:
+            print("***Cliente caido***")
+            break
+    client[i].close()
 
-
-hilo1.start()
-hilo2.start()
-
+a=0
+while True:
+    if a<len(client):
+        hilo=threading.Thread(target=aceptar, args=(a,))
+        hilo.start()
+        a+=1
 
